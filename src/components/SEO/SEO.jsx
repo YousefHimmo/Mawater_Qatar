f52d3d86dfba7896cf034ba357a7e5e27c600f2e
Yousif Himmo <Yousifh@isb.sa>
2025-05-08 09:41:15 +0300

    first upload

---
+++ src/components/SEO/SEO.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const metadata = {
+  title: "Mawater - Buy & Sell Marketplace | Find Everything You Need",
+  description:
+    "Discover Mawater, your ultimate online marketplace for buying and selling a wide range of products. Enjoy a seamless shopping experience with diverse listings, secure transactions, and excellent customer support. Join Mawater today and start exploring!",
+  // keywords: "buy and sell marketplace, online marketplace, Mawater, buy and sell online, online shopping, sell products online, buy products online, secure transactions, online listings, customer support, e-commerce, digital marketplace, shop online, sell online, online buying and selling"
+};
+const SEO = () => {
+  return <></>;
+};
+
+export default SEO;
